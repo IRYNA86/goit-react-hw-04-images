0181d62b311e972625f09b4f5d94062784a1d76a
IRYNA86 <88017665+IRYNA86@users.noreply.github.com>
2022-06-30 15:57:25 +0300

    не приходять картинки великого формату

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import { useState, useEffect } from 'react';
 import { animateScroll as scroll } from 'react-scroll';
 import { ToastContainer } from 'react-toastify';
 import { toast } from 'react-toastify';
@@ -9,118 +9,214 @@ import mapper from '../service/mapper.js';
 import Modal from './Modal/Modal';
 import Button from './Button/Button';
 import * as api from '../service/api';
-import s from '../components/App.module.css'
+import s from '../components/App.module.css';
 
 let totalHits = 0;
 let sumHits = 0;
+const per_page = 12;
 
-class App extends Component {
-  state = {
-    images: [],
-    page: 1,
-    per_page: 12,
-    nameImages: '',
-    isLoading: false,
-    error: null,
-    showModal: false,
-    bigImage: '',
-  };
+function App() {
+  const [images, setImages] = useState([]);
+  const [page, setPage] = useState(1);
+  const [nameImages, setNameImages] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+  // const [error, setError] = useState(null);
+  const [showModal, setShowModal] = useState(false);
+  const [bigImage, setBigImage] = useState('');
 
-  async componentDidUpdate(prevProps, prevState) {
-    const { page, nameImages, per_page } = this.state;
+  // state = {
+  //   images: [],
+  //   page: 1,
+  //   per_page: 12,
+  //   nameImages: '',
+  //   isLoading: false,
+  //   error: null,
+  //   showModal: false,
+  //   bigImage: '',
+  // };
 
-    if (prevState.page !== page || prevState.nameImages !== nameImages) {
+  useEffect(() => {
+    if (!nameImages) {
+      return;
+    }
+    const getData = async () => {
       try {
-        this.setState({ isLoading: true });
-
+        setIsLoading(true);
         const images = await api.getCoolImages(page, nameImages, per_page);
         const imagesData = mapper(images.hits);
-
         if (images.hits.length === 0) {
-          this.setState({ isLoading: false });
+          setIsLoading(false);
           toast.error('There are no images matching your search query');
           return;
         }
-        this.setState(prevState => {
-          return {
-            images: [...prevState.images, ...imagesData],
-            isLoading: false,
-          };
-        });
-
+        setImages(prevState => [...prevState, ...imagesData]);
+        setIsLoading(false);
         totalHits = images.totalHits;
         sumHits += images.hits.length;
-
       } catch (error) {
-        this.setState({ error: true, isLoading: false });
+        setIsLoading(false);
+        toast.error('There are no images matching your search query');
       }
-    }
-  }
+    };
+    getData();
+  }, [page, nameImages]);
 
-  handleFormSubmit = queryValue => {
-    this.setState({
-      images: [],
-      isLoading: false,
-      page: 1,
-      nameImages: queryValue,
-    });
+  // async componentDidUpdate(prevProps, prevState) {
+  //   const { page, nameImages, per_page } = this.state;
+
+  //   if (prevState.page !== page || prevState.nameImages !== nameImages) {
+  //     try {
+  //       this.setState({ isLoading: true });
+
+  //       const images = await api.getCoolImages(page, nameImages, per_page);
+  //       const imagesData = mapper(images.hits);
+
+  //       if (images.hits.length === 0) {
+  //         this.setState({ isLoading: false });
+  //         toast.error('There are no images matching your search query');
+  //         return;
+  //       }
+  //       this.setState(prevState => {
+  //         return {
+  //           images: [...prevState.images, ...imagesData],
+  //           isLoading: false,
+  //         };
+  //       });
+
+  //       totalHits = images.totalHits;
+  //       sumHits += images.hits.length;
+
+  //     } catch (error) {
+  //       this.setState({ error: true, isLoading: false });
+  //     }
+  //   }
+  // }
+
+  const handleFormSubmit = queryValue => {
+    setImages([]);
+    setIsLoading(false);
+    setPage(1);
+    setNameImages(queryValue);
+
+    // this.setState({
+    //   images: [],
+    //   isLoading: false,
+    //   page: 1,
+    //   nameImages: queryValue,
+    // });
+
     sumHits = 0;
   };
 
-  toggleModal = () => {
-    this.setState(state => ({
-      showModal: !state.showModal,
-    }));
+  const toggleModal = () => {
+    setShowModal(!showModal);
   };
 
-  openImage = event => {
-    this.toggleModal();
-    this.setState({ bigImage: event });
+  // toggleModal = () => {
+  //   this.setState(state => ({
+  //     showModal: !state.showModal,
+  //   }));
+  // };
+
+  const openImage = () => {
+    toggleModal();
+    setBigImage(bigImage);
+    console.log(setBigImage())
+    
   };
 
-  nextPage = () => {
-    this.setState(prevState => ({
-      page: prevState.page + 1,
-    }));
-    this.scrollWindow();
+  // openImage = event => {
+  //   this.toggleModal();
+  //   this.setState({ bigImage: event });
+  // };
+
+  const nextPage = () => {
+    setPage(prevState => prevState + 1);
+    scrollWindow();
   };
-  scrollWindow = () => {
+
+  // nextPage = () => {
+  //   this.setState(prevState => ({
+  //     page: prevState.page + 1,
+  //   }));
+  //   this.scrollWindow();
+  // };
+
+  const scrollWindow = () => {
     scroll.scrollToBottom({
       offset: 100,
       smooth: true,
     });
   };
 
-  render() {
-    const { images, isLoading, error, nameImages, showModal, bigImage } =
-      this.state;
+  // scrollWindow = () => {
+  //   scroll.scrollToBottom({
+  //     offset: 100,
+  //     smooth: true,
+  //   });
+  // };
 
-    return (
-      <div>
-        <Searchbar onSubmit={this.handleFormSubmit} />
-        {images.length === 0 && <p className={s.hello}> You can find new picture</p>}
-        {error && <p className={s.hello}>Whoops, something went wrong: {error.message}</p>}
-        {isLoading ? (
-         <Loader/>
-        ) : (
-          <ImageGallery
-            images={images}
-            nameImages={nameImages}
-            onClick={this.openImage}
-          />
-        )}
-        <ToastContainer position="top-right" autoClose={3000} />
-        {showModal && (
-          <Modal onClose={this.toggleModal}>
-           <img src={bigImage} alt="" />
-          </Modal>
-        )}
-        {totalHits !== sumHits && images.length > 0 && (
-          <Button nextPage={this.nextPage} />
-        )}
-      </div>
-    );
-  }
+  return (
+    <div>
+      <Searchbar onSubmit={handleFormSubmit} />
+      {images.length === 0 && (
+        <p className={s.hello}> You can find new picture</p>
+      )}
+      {/* {error && <p className={s.hello}>Whoops, something went wrong: {error.message}</p>} */}
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <ImageGallery
+          images={images}
+          nameImages={nameImages}
+          onClick={openImage}
+        />
+      )}
+      <ToastContainer position="top-right" autoClose={3000} />
+      {showModal && (
+        <Modal onClose={toggleModal}
+        
+        >
+                   <img src={bigImage} alt=""/>
+        </Modal>
+      )}
+      {totalHits !== sumHits && images.length > 0 && (
+        <Button nextPage={nextPage} />
+      )}
+    </div>
+  );
 }
 
+//   render() {
+//     const { images, isLoading, error, nameImages, showModal, bigImage } =
+//       this.state;
+
+//     return (
+//       <div>
+//         <Searchbar onSubmit={this.handleFormSubmit} />
+//         {images.length === 0 && <p className={s.hello}> You can find new picture</p>}
+//         {error && <p className={s.hello}>Whoops, something went wrong: {error.message}</p>}
+//         {isLoading ? (
+//          <Loader/>
+//         ) : (
+//           <ImageGallery
+//             images={images}
+//             nameImages={nameImages}
+//             onClick={this.openImage}
+//           />
+//         )}
+//         <ToastContainer position="top-right" autoClose={3000} />
+//         {showModal && (
+//           <Modal onClose={this.toggleModal}>
+//            <img src={bigImage} alt="" />
+//           </Modal>
+//         )}
+//         {totalHits !== sumHits && images.length > 0 && (
+//           <Button nextPage={this.nextPage} />
+//         )}
+//       </div>
+//     );
+//   }
+// }
+// }
 export default App;
--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,34 +1,90 @@
-import { Component } from 'react';
+import { useEffect } from 'react';
+import PropTypes from 'prop-types';
 import s from '../Modal/Modal.module.css';
 
-class Modal extends Component {
-  componentDidMount() {
-    window.addEventListener('keydown', this.handleKeyDown);
-  }
+function Modal({ children, onClose }) {
+  useEffect(() => {
+    window.addEventListener('keydown', handleKeyDown);
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  });
 
-  componentWillUnmount() {
-    window.removeEventListener('keydown', this.handleKeyDown);
-  }
+  // componentDidMount() {
+  //   window.addEventListener('keydown', this.handleKeyDown);
+  // }
 
-  handleKeyDown = event => {
+  // componentWillUnmount() {
+  //   window.removeEventListener('keydown', this.handleKeyDown);
+  // }
+  const handleKeyDown = event => {
     if (event.code === 'Escape') {
-      this.props.onClose();
+      onClose();
     }
   };
 
-  handleBackdropClick = event => {
+  // handleKeyDown = event => {
+  //   if (event.code === 'Escape') {
+  //     this.props.onClose();
+  //   }
+  // };
+
+  const handleBackdropClick = event => {
     if (event.currentTarget === event.target) {
-      this.props.onClose();
+      onClose();
     }
   };
-  render() {
-    const {children} = this.props
-    return (
-      <div className={s.overlay} onClick={this.handleBackdropClick}>
-        <div className={s.modal}>{children}</div>
-      </div>
-    );
-  }
+
+  // handleBackdropClick = event => {
+  //   if (event.currentTarget === event.target) {
+  //     this.props.onClose();
+  //   }
+  // };
+
+  return (
+    <div className={s.overlay} onClick={handleBackdropClick}>
+      <div className={s.modal}>
+        
+        {children}
+        
+        </div>
+    </div>
+  );
 }
+
+// class Modal extends Component {
+//   componentDidMount() {
+//     window.addEventListener('keydown', this.handleKeyDown);
+//   }
+
+//   componentWillUnmount() {
+//     window.removeEventListener('keydown', this.handleKeyDown);
+//   }
+
+//   handleKeyDown = event => {
+//     if (event.code === 'Escape') {
+//       this.props.onClose();
+//     }
+//   };
+
+//   handleBackdropClick = event => {
+//     if (event.currentTarget === event.target) {
+//       this.props.onClose();
+//     }
+//   };
+//   render() {
+//     const {children} = this.props
+//     return (
+//       <div className={s.overlay} onClick={this.handleBackdropClick}>
+//         <div className={s.modal}>{children}</div>
+//       </div>
+//     );
+//   }
+// }
+
+Modal.propTypes = {
+  children: PropTypes.element.isRequired,
+  onClose: PropTypes.func.isRequired,
+};
 
 export default Modal;
